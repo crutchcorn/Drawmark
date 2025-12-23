b6c2c783c915f3d793abcdb7364e40a3f9460d6f
Corbin Crutchley <git@crutchcorn.dev>
2025-12-23 01:46:29 -0800

    chore: fix loading RN app from new paths

--- apps/drawmark/metro.config.js
+++ apps/drawmark/metro.config.js
@@ -1,8 +1,30 @@
+// Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require('expo/metro-config');
+const { FileStore } = require('metro-cache');
+const path = require('node:path');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
 
 config.resolver.sourceExts.push('sql'); // <--- add this
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+];
+// #3 - Force resolving nested modules to the folders below
+config.resolver.disableHierarchicalLookup = true;
+
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+    new FileStore({ root: path.join(projectRoot, 'node_modules', '.cache', 'metro') }),
+];
 
 module.exports = config;
